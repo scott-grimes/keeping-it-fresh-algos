251a16886462b06479791304c36a03660acaeb21
Scott <scottgrimes89@gmail.com>
2018-09-13 09:17:36 -0500

    formatting

--- alphabeticAnagrams/index.js
+++ alphabeticAnagrams/index.js
@@ -15,90 +15,85 @@ The function should be able to accept any word 20 letters or less in length (pos
 */
 
 
-const anagramPosition = string => {
- 
-  const fact = z => { ans = 1; for (i = 1; i < z + 1; i++) { ans *= i }; return !z ? 0 : ans }
+const anagramPosition = (string) => {
+  const fact = (z) => { ans = 1; for (i = 1; i < z + 1; i++) { ans *= i; } return !z ? 0 : ans; };
 
-  let orig = string.split('');
-  let uniques = Array.from(new Set(orig)).sort();
-  let positionDict = {};
+  const orig = string.split('');
+  const uniques = Array.from(new Set(orig)).sort();
+  const positionDict = {};
 
   uniques.forEach((letter, index) => {
     positionDict[letter] = 1 + index;
-  })
+  });
 
   // how many characters positioned after the index in the original string come before the character in the alphabet?
   const numerator = orig.map((c, index) => {
-
     let count = 0;
-    const charsInFront = uniques.filter(char => positionDict[char] < positionDict[c])
+    const charsInFront = uniques.filter(char => positionDict[char] < positionDict[c]);
 
     if (!charsInFront.length) {
       return count;
     }
 
-    stringAfterMe = orig.slice(index);
+    const stringAfterMe = orig.slice(index);
 
-    stringAfterMe.forEach(char => {
+    stringAfterMe.forEach((char) => {
       if (char !== c && charsInFront.includes(char)) {
         count++;
       }
-    })
-    return count
-  })
+    });
+    return count;
+  });
 
 
   // multiply factorial(letterfrequency) for each letter in the string from the given index on
   const denominator = orig.map((c, index) => {
     // string from each character onward
-    stringAfterMe = orig.slice(index);
+    const stringAfterMe = orig.slice(index);
 
     // number of repeats for all characters in string after me
-    let repeatedDict = {}
-    stringAfterMe.forEach(c => {
+    const repeatedDict = {};
+    stringAfterMe.forEach((c) => {
       if (!repeatedDict[c]) {
         repeatedDict[c] = 0;
       }
       repeatedDict[c]++;
-    })
+    });
 
-    //x!,y!,z! for all characters after me
+    // x!,y!,z! for all characters after me
 
-    const numArr = Object.values(repeatedDict).map(x => fact(x))
+    const numArr = Object.values(repeatedDict).map(x => fact(x));
 
     // multiply together x!*y!*z!
     return numArr.reduce((m, i) => m * i, 1);
   });
 
   // 5!, 4!, 3!, 2!, 1!, 0! for each character in the array
-  const reverseFact = new Array(orig.length).fill(0).map((x, i) => fact(i)).reverse()
+  const reverseFact = new Array(orig.length).fill(0).map((x, i) => fact(i)).reverse();
 
-  const comboArr = numerator.map((x, i) => {
-    return x * reverseFact[i] / denominator[i];
-  })
+  const comboArr = numerator.map((x, i) => x * reverseFact[i] / denominator[i]);
 
   // return sum, plus 1 for indexing at 1 instead of zero
   return comboArr.reduce((m, i) => i + m, 0) + 1;
-}
+};
 
-const assert = (input, actual, expected) =>{
+const assert = (input, actual, expected) => {
   if (actual === expected) {
-    return console.log(`${input} was ${actual} ✓`)
+    return console.log(`${input} was ${actual} ✓`);
   }
 
   throw new Error(`${input} expected ${expected} but got ${actual}`);
-}
-  
+};
+
 
 try {
-  const inputs = ["ARCTIC","BAEBEE","TURQUOISE","STARK","ABBS","MISSISSIPPI","BABS","PARIS"];
-  const expecteds = [42,12,139432,92,1,13737,4,51]
-  
-  inputs.forEach((input,index)=>assert(input,anagramPosition(input),expecteds[index]))
+  const inputs = ['ARCTIC', 'BAEBEE', 'TURQUOISE', 'STARK', 'ABBS', 'MISSISSIPPI', 'BABS', 'PARIS'];
+  const expecteds = [42, 12, 139432, 92, 1, 13737, 4, 51];
 
-  console.log('All Tests Passed!')
+  inputs.forEach((input, index) => assert(input, anagramPosition(input), expecteds[index]));
 
-}catch(err){
-  console.log(err)
-  console.log('Failed!')
+  console.log('All Tests Passed!');
+} catch (err) {
+  console.log(err);
+  console.log('Failed!');
 }
